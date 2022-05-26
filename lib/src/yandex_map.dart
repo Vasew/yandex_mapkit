f4d80f4d54771e90d2b21125987b27ffdab10f67
Vasew <vasiliykolesov@yahoo.com.au>
2022-05-26 10:34:54 +0300

    Update yandex_map.dart

--- lib/src/yandex_map.dart
+++ lib/src/yandex_map.dart
@@ -65,7 +65,7 @@ class _YandexMapState extends State<YandexMap> {
     } else if(kIsWeb) {
       var centerPoint;
       var centerPointWeb;
-      print("! MAP WEB - ${widget.mapObjectsWeb.first.latitude}");
+      print("! MAP WEB - ${widget.mapObjectsWeb.first['longitude']}");
       /*if(widget.mapObjectsWeb.isNotEmpty){
         var mapObjectsJsonWeb = widget.mapObjectsWeb.first.toJson();
         centerPointWeb = [mapObjectsJsonWeb['point']['latitude'], mapObjectsJsonWeb['point']['longitude']];
