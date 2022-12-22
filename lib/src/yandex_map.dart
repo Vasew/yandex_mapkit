3878dcba20bbf376a8ba73c5d511268b11d4f0ef
Vasew <vasiliykolesov@yahoo.com.au>
2022-12-22 15:11:18 +0300

    Update yandex_map.dart

--- lib/src/yandex_map.dart
+++ lib/src/yandex_map.dart
@@ -329,7 +329,9 @@ class _YandexMapState extends State<YandexMap> {
                 objectManager = new ymaps.ObjectManager({
                     clusterize: true,
                     gridSize: 32,
-                    clusterDisableClickZoom: true
+                    clusterDisableClickZoom: true,
+                    geoObjectOpenBalloonOnClick: false,
+                    clusterOpenBalloonOnClick: false
                 });
 
             myMap.controls.add('zoomControl', {
